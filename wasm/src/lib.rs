cfc302a1bd9835e0e03b474b5928759b9b261121
kazukiyoshida <kazukiyoshida@kazuki-macbook.local>
2020-03-03 21:02:46 +0900

    feat: update sample wasm module

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,9 +1,14 @@
-extern crate wasm_bindgen;
-use wasm_bindgen::prelude::*;
+#[macro_use]
+extern crate downcast_rs;
+use downcast_rs::DowncastSync;
 
-extern crate circuit_simulator;
+use wasm_bindgen::prelude::*;
 use circuit_simulator::circuit::*;
 use circuit_simulator::elements::*;
+use avr_emulator::atmega328p::*;
+use avr_emulator::avr::*;
+
+pub const SAMPLE_FILE_NAME: &str = "hex/atmel_studio/led_flashing_fast/led_flashing.hex";
 
 #[wasm_bindgen]
 pub enum Element {
@@ -28,38 +33,98 @@ impl CircuitController {
         }
     }
 
-    pub fn exec_test(&mut self) -> String {
-        self.scenario_test()
+    pub fn exec_test(&mut self) {
+        self.scenario1();
     }
 }
 
 impl CircuitController {
-    // シナリオテスト
-    pub fn scenario_test(&mut self) -> String {
-        let r = Resistor::new(
-            "R1".to_string(),
-            330_f32,
-            [Some("N1".to_string()), Some("N2".to_string())]
-        );
+
+    // シナリオテスト1.
+    // 5V 電源 - 抵抗 - LED - GND の定常回路
+    pub fn scenario1(&mut self) {
+        // let ten_millis = time::Duration::from_millis(100);
+
+        // 回路の作成
         let v = IndependentVoltageSource {
-            name: "V1".to_string(),
+            name: "V".to_string(),
             v: 5_f32,
             nodes: [Some("N1".to_string()), Some(GND.to_string())]
         };
+        let r = Resistor::new(
+            "R".to_string(),
+            330_f32,
+            [Some("N1".to_string()), Some("N2".to_string())]
+        );
         let d = Diode::new(
-            "Diode1".to_string(),
+            "LED".to_string(),
             [Some("N2".to_string()), Some(GND.to_string())]
         );
-
-        self.circuit.add(Box::new(r));
         self.circuit.add(Box::new(v));
+        self.circuit.add(Box::new(r));
         self.circuit.add(Box::new(d));
 
-        let vec = self.circuit.solve_eq();
-        if let Some(vec) = vec {
-            format!("{}", vec)
-        } else {
-            "error!".to_string()
+        println!("|||||||||||||||||||");
+        // CPU の準備
+        let avr = ATmega328P::new();
+
+        let mut timer0 = avr.new_timer0();
+        let mut timer1 = avr.new_timer1();
+        let mut timer2 = avr.new_timer2();
+        let mut portb = avr.new_portb();
+        let mut portc = avr.new_portc();
+        let mut portd = avr.new_portd();
+
+        avr.load_hex(SAMPLE_FILE_NAME);
+        avr.initialize_sram();
+
+        let mut last_pinb = 0;
+
+        // CPU 起動
+        loop {
+            avr.execute();
+            timer0.clk_io();
+            timer1.clk_io();
+            timer2.clk_io();
+            portb.clk_io();
+            portc.clk_io();
+            portd.clk_io();
+
+            if last_pinb != portb.pinx() {
+                if bit(portb.pinx(), 5) {
+                    if let Some(source) = self.circuit.elements[0].downcast_mut::<IndependentVoltageSource>() {
+                        source.v = 5f32;
+                    }
+                } else {
+                    if let Some(source) = self.circuit.elements[0].downcast_mut::<IndependentVoltageSource>() {
+                        source.v = 0f32;
+                    }
+                }
+
+                println!("-----------------------------------");
+                println!("{:?}", self.circuit);
+
+                // IndVoltageSrc
+
+
+                last_pinb = portb.pinx()
+            }
+
+            // if avr.cycle() % 1000 == 0 {
+            //     println!("cycle = {:10}    PORTB = {}", avr.cycle(), portb,);
+            // }
         }
     }
+
 }
+
+#[test]
+fn test_scenario1() {
+    let mut c = CircuitController::new();
+    c.scenario1();
+}
+
+pub fn bit(a: u8, n: u8) -> bool {
+    ((a & 1 << n) >> n) == 1
+}
+
